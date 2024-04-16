1775b95eb4e475d7bd5b870e234cc47fcf519a15
JavaNoTea <christiankingcompsci@gmail.com>
2024-04-16 15:19:25 -0400

    Allele graph fixed

--- bargraph.py
+++ bargraph.py
@@ -1,7 +1,10 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-data = pd.read_csv('allele.csv')
+data = pd.read_csv('allele.csv', header=None, names=['gene', 'allele', 'frequency'])
+
+data['gene'] = data['gene'].astype(str)
+data['allele'] = data['allele'].astype(str)
 
 data['Label'] = data['gene'] + ', ' + data['allele']
 
@@ -9,7 +12,8 @@ plt.figure(figsize=(10, 8))
 plt.bar(data['Label'], data['frequency'], color='blue')
 plt.xlabel('Gene, Allele')
 plt.ylabel('Frequency')
-plt.title('Frequency of Gene-Allele Combinations')
+plt.title('Frequency of Alleles')
 plt.xticks(rotation=45)
 plt.tight_layout()
 plt.savefig('gene_allele_frequency_chart.png', format='png', dpi=300)
+
